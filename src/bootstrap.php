61134383fb7d79949299d4b75387d392a7130ba6
hdj <hdj@addcn.com>
2020-11-02 08:38:43 +0800

    add update-bootstrap script

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,46 +1,39 @@
 <?php
 
 $baseDir = __DIR__;
-
-$classmap = array(
-    'JetConsulporterInterface'   => $baseDir . '/Contract/JetConsulporterInterface.php',
-    'JetDataFormatterInterface'  => $baseDir . '/Contract/JetDataFormatterInterface.php',
-    'JetPackerInterface'         => $baseDir . '/Contract/JetPackerInterface.php',
-    'JetPathGeneratorInterface'  => $baseDir . '/Contract/JetPathGeneratorInterface.php',
-    'JetLoadBalancerInterface'   => $baseDir . '/Contract/JetLoadBalancerInterface.php',
-    'JetTransporterInterface'    => $baseDir . '/Contract/JetTransporterInterface.php',
-
-    'JetConsulClient'            => $baseDir . '/Consul/JetConsulClient.php',
-    'JetConsulHealth'            => $baseDir . '/Consul/JetConsulHealth.php',
-
-    'JetClientException'         => $baseDir . '/Exception/JetClientException.php',
-    'JetServerException'         => $baseDir . '/Exception/JetServerException.php',
-    'JetRecvFailedException'     => $baseDir . '/Exception/JetRecvFailedException.php',
-
-    'Jet'                        => $baseDir . '/Jet.php',
-    'AbstractJetClient'          => $baseDir . '/AbstractJetClient.php',
-    'JetClient'                  => $baseDir . '/JetClient.php',
-    'JetUtil'                    => $baseDir . '/JetUtil.php',
-
-    'AbstractJetLoadBalancer'    => $baseDir . '/LoadBalancer/AbstractJetLoadBalancer.php',
-    'JetRoundRobinLoadBalancer'  => $baseDir . '/LoadBalancer/JetRoundRobinLoadBalancer.php',
-    'JetRandomLoadBalancer'      => $baseDir . '/LoadBalancer/JetRandomLoadBalancer.php',
-    'JetLoadBalancerNode'        => $baseDir . '/LoadBalancer/JetLoadBalancerNode.php',
-
-    'AbstractJetTransporter'     => $baseDir . '/Transporter/AbstractJetTransporter.php',
-    'JetCurlHttpTransporter'     => $baseDir . '/Transporter/JetCurlHttpTransporter.php',
-    'JetStreamSocketTransporter' => $baseDir . '/Transporter/JetStreamSocketTransporter.php',
-    'JetJsonEofPacker'           => $baseDir . '/Packer/JetJsonEofPacker.php',
-    'JetJsonLengthPacker'        => $baseDir . '/Packer/JetJsonLengthPacker.php',
-
-    'JetDataFormatter'           => $baseDir . '/DataFormatter/JetDataFormatter.php',
-
-    'JetPathGenerator'           => $baseDir . '/PathGenerator/JetPathGenerator.php',
-    'JetConsulporter'            => $baseDir . '/Consulporter/JetConsulporter.php',
+$classMap = array(
+    'JetConsulporterInterface' => $baseDir . '/src/Contract/JetConsulporterInterface.php',
+    'JetDataFormatterInterface' => $baseDir . '/src/Contract/JetDataFormatterInterface.php',
+    'JetPackerInterface' => $baseDir . '/src/Contract/JetPackerInterface.php',
+    'JetPathGeneratorInterface' => $baseDir . '/src/Contract/JetPathGeneratorInterface.php',
+    'JetLoadBalancerInterface' => $baseDir . '/src/Contract/JetLoadBalancerInterface.php',
+    'JetTransporterInterface' => $baseDir . '/src/Contract/JetTransporterInterface.php',
+    'JetConsulporter' => $baseDir . '/src/Consulporter/JetConsulporter.php',
+    'AbstractJetClient' => $baseDir . '/src/AbstractJetClient.php',
+    'JetCurlHttpTransporter' => $baseDir . '/src/Transporter/JetCurlHttpTransporter.php',
+    'JetStreamSocketTransporter' => $baseDir . '/src/Transporter/JetStreamSocketTransporter.php',
+    'AbstractJetTransporter' => $baseDir . '/src/Transporter/AbstractJetTransporter.php',
+    'JetRoundRobinLoadBalancer' => $baseDir . '/src/LoadBalancer/JetRoundRobinLoadBalancer.php',
+    'AbstractJetLoadBalancer' => $baseDir . '/src/LoadBalancer/AbstractJetLoadBalancer.php',
+    'JetRandomLoadBalancer' => $baseDir . '/src/LoadBalancer/JetRandomLoadBalancer.php',
+    'JetLoadBalancerNode' => $baseDir . '/src/LoadBalancer/JetLoadBalancerNode.php',
+    'JetClient' => $baseDir . '/src/JetClient.php',
+    'bootstrap' => $baseDir . '/src/bootstrap.php',
+    'JetPathGenerator' => $baseDir . '/src/PathGenerator/JetPathGenerator.php',
+    'Jet' => $baseDir . '/src/Jet.php',
+    'JetConsulClient' => $baseDir . '/src/Consul/JetConsulClient.php',
+    'JetConsulHealth' => $baseDir . '/src/Consul/JetConsulHealth.php',
+    'JetUtil' => $baseDir . '/src/JetUtil.php',
+    'JetServerException' => $baseDir . '/src/Exception/JetServerException.php',
+    'JetClientException' => $baseDir . '/src/Exception/JetClientException.php',
+    'JetRecvFailedException' => $baseDir . '/src/Exception/JetRecvFailedException.php',
+    'JetDataFormatter' => $baseDir . '/src/DataFormatter/JetDataFormatter.php',
+    'JetJsonEofPacker' => $baseDir . '/src/Packer/JetJsonEofPacker.php',
+    'JetJsonLengthPacker' => $baseDir . '/src/Packer/JetJsonLengthPacker.php',
 );
 
-spl_autoload_register(function ($class) use ($classmap) {
-    if (isset($classmap[$class]) && is_file($classmap[$class])) {
-        require_once $classmap[$class];
+spl_autoload_register(function ($class) use ($classMap) {
+    if (isset($classMap[$class]) && is_file($classMap[$class])) {
+        require_once $classMap[$class];
     }
 });
---
+++ update-bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+BOOTSTRAP=`dirname $(realpath $0)`/src/bootstrap.php
+
+cat <<EOT > ${BOOTSTRAP}
+<?php
+
+\$baseDir = __DIR__;
+\$classMap = array(
+EOT
+
+for FILE in `find ./src -type f -name "*.php"`; do
+    REALPATH=`echo ${FILE} |sed -r "s/^\.//"`
+    FILENAME="$(basename -- $FILE)"
+    CLASS="${FILENAME%.php}"
+    CLASSMAP="'${CLASS}' => \$baseDir . '${REALPATH}',"
+    echo "    ${CLASSMAP}" >> ${BOOTSTRAP}
+done
+
+cat <<EOT >> ${BOOTSTRAP}
+);
+
+spl_autoload_register(function (\$class) use (\$classMap) {
+    if (isset(\$classMap[\$class]) && is_file(\$classMap[\$class])) {
+        require_once \$classMap[\$class];
+    }
+});
+EOT
