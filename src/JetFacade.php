1f884dad250c59b0d64bb0752baae63bcb3f81d5
hdj <hdj@addcn.com>
2020-11-05 13:41:28 +0800

    optimize JetFacade

--- src/JetFacade.php
+++ src/JetFacade.php
@@ -3,6 +3,41 @@
 abstract class JetFacade
 {
     /**
+     * @var array
+     */
+    protected static $instances = array();
+
+    /**
+     * @return JetClient
+     * @throws RuntimeException
+     * @throws InvalidArgumentException
+     * @throws Exception
+     */
+    protected static function getFacadeRoot()
+    {
+        return static::resolveFacadeInstance(static::getFacadeAccessor());
+    }
+
+    /**
+     * @param JetClient|string $name
+     * @return JetClient
+     * @throws InvalidArgumentException
+     * @throws Exception
+     */
+    protected static function resolveFacadeInstance($name)
+    {
+        if (is_object($name)) {
+            return $name;
+        }
+
+        if (isset(static::$instances[$name])) {
+            return static::$instances[$name];
+        }
+
+        return static::$instances[$name] = JetClientFactory::create($name);
+    }
+
+    /**
      * @return JetClient|string
      * @throws RuntimeException
      */
@@ -18,12 +53,6 @@ abstract class JetFacade
      */
     public static function __callStatic($name, $arguments)
     {
-        $accessor = static::getFacadeAccessor();
-
-        if (is_string($accessor)) {
-            $accessor = JetClientFactory::create($accessor);
-        }
-
-        return call_user_func_array(array($accessor, $name), $arguments);
+        return call_user_func_array(array(static::getFacadeRoot(), $name), $arguments);
     }
 }
