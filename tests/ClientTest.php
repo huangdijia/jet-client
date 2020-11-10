a5288193e48665ed14a4530528bbf89e4a36f227
hdj <hdj@addcn.com>
2020-11-10 10:10:20 +0800

    format and add TestCase

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,42 +2,29 @@
 
 declare(strict_types=1);
 /**
- * This file is part of Hyperf Jet-client.
+ * This file is part of Jet-Client.
  *
  * @link     https://github.com/huangdijia/jet-client
  * @document https://github.com/huangdijia/jet-client/blob/main/README.md
  * @contact  huangdijia@gmail.com
  * @license  https://github.com/huangdijia/jet-client/blob/main/LICENSE
  */
-namespace Huangdijia\Jet\Test;
+namespace Huangdijia\Jet\Tests;
 
 use Huangdijia\Jet\ClientFactory;
-use Huangdijia\Jet\Registry\ConsulRegistry;
 use Huangdijia\Jet\ServiceManager;
-use Huangdijia\Jet\Transporter\GuzzleHttpTransporter;
-use PHPUnit\Framework\TestCase;
 
 /**
  * @internal
  * @coversNothing
  */
-class ServiceTest extends TestCase
+class ClientTest extends TestCase
 {
     private $service = 'CalculatorService';
 
-    private $registryHost = '127.0.0.1';
-
-    private $registryPort = 8500;
-
-    private $serviceHost = '127.0.0.1';
-
-    private $serviceHttpPort = 9502;
-
-    private $serviceTcpPort = 9503;
-
     public function testCalculatorServiceByRegistry()
     {
-        $registry = new ConsulRegistry($this->registryHost, $this->registryPort, 1);
+        $registry = $this->createRegistry();
 
         ServiceManager::register($this->service, [
             ServiceManager::REGISTRY => $registry,
@@ -51,9 +38,9 @@ class ServiceTest extends TestCase
         $this->assertSame($a + $b, $client->add($a, $b));
     }
 
-    public function testCalculatorServiceByHttpTransporter()
+    public function testCalculatorServiceByGuzzleHttpTransporter()
     {
-        $client = ClientFactory::create($this->service, new GuzzleHttpTransporter($this->serviceHost, $this->serviceHttpPort));
+        $client = ClientFactory::create($this->service, $this->createGuzzleHttpTransporter());
 
         $a = rand(1, 99);
         $b = rand(1, 99);
@@ -61,9 +48,9 @@ class ServiceTest extends TestCase
         $this->assertSame($a + $b, $client->add($a, $b));
     }
 
-    public function testCalculatorServiceByTcpTransporter()
+    public function testCalculatorServiceByStreamSocketTransporter()
     {
-        $client = ClientFactory::create($this->service, new GuzzleHttpTransporter($this->serviceHost, $this->serviceTcpPort));
+        $client = ClientFactory::create($this->service, $this->createStreamSocketTransporter());
 
         $a = rand(1, 99);
         $b = rand(1, 99);
